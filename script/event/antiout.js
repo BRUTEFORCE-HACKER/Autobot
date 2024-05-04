d333b637847774d00abbb31b1a6f83608d94983b
BRUTEFORCE-HACKER <84610202+BRUTEFORCE-HACKER@users.noreply.github.com>
2024-05-04 20:30:09 +0800

    Update antiout.js

--- script/event/antiout.js
+++ script/event/antiout.js
@@ -1,23 +1,19 @@
 module.exports.config = {
-  name: "antiout",
-  version: "1.0.0"
+ name: "antiout",
+ version: "0.0.1"
 };
-module.exports.handleEvent = async ({
-  event,
-  api
-}) => {
-  if (event.logMessageData?.leftParticipantFbId === api.getCurrentUserID()) return;
-  if (event.logMessageData?.leftParticipantFbId) {
-    const info = await api.getUserInfo(event.logMessageData?.leftParticipantFbId);
-    const {
-      name
-    } = info[event.logMessageData?.leftParticipantFbId];
-    api.addUserToGroup(event.logMessageData?.leftParticipantFbId, event.threadID, (error) => {
-      if (error) {
-        api.sendMessage(`Unable to re-add member ${name} to the group!`, event.threadID);
-      } else {
-        api.sendMessage(`Active antiout mode, ${name} has been re-added to the group successfully!`, event.threadID);
-      }
-    });
-  }
-};
+
+module.exports.run = async({ event, api, Threads, Users }) => {
+ let data = (await Threads.getData(event.threadID)).data || {};
+ if (data.antiout == false) return;
+ if (event.logMessageData.leftParticipantFbId == api.getCurrentUserID()) return;
+ const name = global.data.userName.get(event.logMessageData.leftParticipantFbId) || await Users.getNameUser(event.logMessageData.leftParticipantFbId);
+ const type = (event.author == event.logMessageData.leftParticipantFbId) ? "self-separation" : "being kicked by the administrator";
+ if (type == "self-separation") {
+  api.addUserToGroup(event.logMessageData.leftParticipantFbId, event.threadID, (error, info) => {
+   if (error) {
+    api.sendMessage(`Unable to re-add member ${name} to the group. Putangina mo wag kana babalik`, event.threadID)
+   } else api.sendMessage(`Active antiout mode, ${name} wala kang takas sakin :>`, event.threadID);
+  })
+ }
+}
